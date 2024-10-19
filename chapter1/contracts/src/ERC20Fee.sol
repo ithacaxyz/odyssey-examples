f6f6c02937c088128ab4b606956b5b3fcbfe7f1e
Qi Zhou <qizhou@ethstorage.io>
2024-10-19 09:20:02 +0800

    add feeToken to ERC20 as fee tutorial

--- chapter1/contracts/src/ERC20Fee.sol
+++ chapter1/contracts/src/ERC20Fee.sol
@@ -26,13 +26,13 @@ contract ERC20Fee {
     uint256 public nonce;
 
     /// @notice Main entrypoint to send tx.
-    function sendERC20(IERC20 token, address to, uint256 amount, uint256 fee, uint8 v, bytes32 r, bytes32 s) public {
-        bytes32 digest = keccak256(abi.encode(nonce++, token, to, amount, fee));
+    function sendERC20(IERC20 token, address to, uint256 amount, IERC20 feeToken, uint256 fee, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 digest = keccak256(abi.encode(nonce++, token, to, amount, feeToken, fee));
         address addr = ecrecover(digest, v, r, s);
 
         require(addr == address(this));
 
-        require(token.transfer(msg.sender, fee));
+        require(feeToken.transfer(msg.sender, fee));
         require(token.transfer(to, amount));
     }
 }
